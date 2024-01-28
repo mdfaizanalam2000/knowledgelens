0c1a66a1c4cf52146618f8cf42e3464b88649555
MD Faizan Alam <alamf015@gmail.com>
2024-01-28 22:03:00 +0530

    added class and methods

--- api.py
+++ api.py
@@ -1,119 +1,68 @@
-from server import connect_to_server
+from server import Server
 from fastapi import FastAPI,Body
 
 app=FastAPI()
 
-cursor=connect_to_server()
+server=Server()
+server.connect_to_server()
+
+# ------------API ROUTES FOR HANDLING TASKS----------
 
 @app.post("/addTask")
-def addTask(task=Body()):
-   try:
-    query=f"insert into task_manager values({task['task_id']},{task['user_id']},'{task['title']}','{task['description']}','{task['due_date']}','{task['status']}');"
-
-    cursor.execute(query).commit()
-    return {"message":"success, task created!"}
-   except Exception as e:
-       return {"message":str(e)}
+def root(task=Body()):
+   res=server.addTask(task)
+   return res
 
 @app.get("/getAllTasks")
-def getAllTasks():
-    try:
-        query="select * from task_manager;"
-        tasks=cursor.execute(query).fetchall()
-        cname= [column[0] for column in cursor.description]
-        result=[dict(zip(cname,result)) for result in tasks]
-        return result
-    except Exception as e:
-       return {"message":str(e)}
+def root():
+    res=server.getAllTasks()
+    return res
     
 @app.get("/getTask")
-def getTask(task_id:int):
-    try:
-        query=f"select * from task_manager where task_id={task_id};"
-        data=cursor.execute(query).fetchone()
-        cname= [column[0] for column in cursor.description]
-        result=[dict(zip(cname,data))]
-        return result
-    except Exception as e:
-       return {"message":str(e)}
+def root(task_id:int):
+    res=server.getTaskByID(task_id)
+    return res
    
 @app.put("/updateTask")
-def updateTask(task_id:int,updated_task=Body()):
-   try:          
-    query=f"update task_manager set title='{updated_task['title']}',description='{updated_task['description']}',due_date='{updated_task['due_date']}',status='{updated_task['status']}' where task_id={task_id};"
-
-    cursor.execute(query).commit()
-    return {"message":"success, task updated!"}
-   except Exception as e:
-       return {"message":str(e)}
+def root(task_id:int,updated_task=Body()):
+   res=server.updateTaskByID(task_id,updated_task)
+   return res
    
 @app.delete("/deleteTask")
-def deleteTask(task_id:int):
-   try:
-    query=f"delete from task_manager where task_id={task_id};"
-    cursor.execute(query).commit()
-    return {"message":"task deleted successfully!"}
-   except Exception as e:
-       return {"message":str(e)}
+def root(task_id:int):
+   res=server.deleteTaskByID(task_id)
+   return res
 
-# ---------------USER API ROUTES---------------
+# ---------------API ROUTES FOR HANDLING USERS---------------
 
 @app.post("/addUser")
-def addUser(user=Body()):
-   try:
-    query=f"insert into user_data values({user['user_id']},'{user['name']}','{user['designation']}');"
-
-    cursor.execute(query).commit()
-    return {"message":"success, user created!"}
-   except Exception as e:
-       return {"message":str(e)}
+def root(user=Body()):
+   res=server.addUser(user)
+   return res
    
 @app.get("/getUser")
-def getUser(user_id:int):
-    try:
-        query=f"select * from user_data where user_id={user_id};"
-        data=cursor.execute(query).fetchone()
-        cname= [column[0] for column in cursor.description]
-        result=[dict(zip(cname,data))]
-        return result
-    except Exception as e:
-       return {"message":str(e)}
+def root(user_id:int):
+    res=server.getUserByID(user_id)
+    return res
     
 @app.get("/getAllUsers")
-def getAllUsers():
-    try:
-        query="select * from user_data;"
-        users=cursor.execute(query).fetchall()
-        cname= [column[0] for column in cursor.description]
-        result=[dict(zip(cname,result)) for result in users]
-        return result
-    except Exception as e:
-       return {"message":str(e)}
+def root():
+    res=server.getAllUsers()
+    return res
     
 @app.put("/updateUser")
-def updateUser(user_id:int,updated_user=Body()):
-   try:          
-    query=f"update user_data set name='{updated_user['name']}',designation='{updated_user['designation']}' where user_id={user_id};"
-
-    cursor.execute(query).commit()
-    return {"message":"success, user updated!"}
-   except Exception as e:
-       return {"message":str(e)}
+def root(user_id:int,updated_user=Body()):
+   res=server.updateUserByID(user_id,updated_user)
+   return res
     
 # API ROUTE TO FIND ALL TASKS OF A PARTICULAR USER
 @app.get("/getAllTasksByUser")
-def getAllTasksByUser(user_id:int):
-    try:
-        query=f"select * from task_manager t join user_data u on t.user_id=u.user_id where t.user_id={user_id};"
-        
-        tasks=cursor.execute(query).fetchall()
-        cname= [column[0] for column in cursor.description]
-        result=[dict(zip(cname,result)) for result in tasks]
-        return {"tasks":result}
-    except Exception as e:
-       return {"message":str(e)}
+def root(user_id:int):
+    res=server.getAllTasksByUserID(user_id)
+    return res
 
+# TERMINATING CONNECTION ON SHUTDOWN
 @app.on_event("shutdown")
 def close_connection():
-   cursor.close()
+   server.cursor.close()
    print("connection closed")
